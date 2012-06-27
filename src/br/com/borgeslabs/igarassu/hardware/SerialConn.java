4a9067ffa52f3b1124096b1d816177d382c891f2
Paulo Borges <pauloborgesfilho@gmail.com>
2012-06-27 14:25:19 -0300

    New baud rate for serial communication
    The default baud rate is now 38400. The former baud rate did not work
    well in Windows.

--- src/br/com/borgeslabs/igarassu/hardware/SerialConn.java
+++ src/br/com/borgeslabs/igarassu/hardware/SerialConn.java
@@ -1,12 +1,11 @@
 package br.com.borgeslabs.igarassu.hardware;
 
-import br.com.borgeslabs.igarassu.instrument.Pad;
 import processing.serial.Serial;
 
 public class SerialConn extends Hardware {
     public static final String type = "Serial";
     public static final int ENCLOSING_MSG_CHAR = '\n';
-    public static final int BAUD = 57600;
+    public static final int BAUD = 38400;
     
     private static String _msg;
     private static Serial _serial;
@@ -42,8 +41,6 @@ public class SerialConn extends Hardware {
         try {
             int idPad = Integer.parseInt(_msg.substring(0, comma));
             int intensity = Integer.parseInt(_msg.substring(comma+1, newline-1));
-            
-            //System.out.println("Pad: " + idPad + " Intensidade: " + intensity);
     
             this.instrument.triggerPad(idPad, intensity, timestamp);
         } catch (Exception e) {
